2ffab759dc99ab15c53d24791306a2ae17a9ea78
Kai Shiao <kshiao13@gmail.com>
2016-03-09 12:03:21 -0800

    Ensured smooth transitions from one window to the next.

--- jquery/finaljeopardy.js
+++ jquery/finaljeopardy.js
@@ -1,5 +1,5 @@
 (function(){
-	var $correctAnswer='SALUTARY NEGLLECT';
+	var $correctAnswer='SALUTARY NEGLECT';
 	var $startTime;
 	var $jeopardyTotalPrize=Number(localStorage.getItem('jeopardyTotalPrize'));
 	var $doubleJeopardyTotalPrize=Number(localStorage.getItem('doubleJeopardyTotalPrize'));
@@ -22,9 +22,9 @@
 		$wagerValue=Number($('#wagerBox').find('input').val());
 		var $notice;
 		
-		if ($beforeFinalJeopardyTotalPrize<0 && ($wagerValue<0 || $wagerValue>(-$beforeFinalJeopardyTotalPrize))){
+		if ($beforeFinalJeopardyTotalPrize<0 && ($wagerValue<0 || $wagerValue>((-$beforeFinalJeopardyTotalPrize)+10000))){
 			$notice='<p>SINCE YOUR CURRENT TOTAL PRIZE WINNINGS ARE BELOW $0, PLEASE ENTER A VALUE EQUAL TO OR GREATER';
-			$notice+=' THAN 0 BUT NOT MORE THAN THE POSITIVE EQUIVALENT OF YOUR CURRENT TOTAL PRIZE WINNINGS.</p>';
+			$notice+=' THAN 0 BUT NOT MORE THAN $10,000.</p>';
 			$('#wagerBox').find('p').eq(1).html($notice);
 		} else if ($beforeFinalJeopardyTotalPrize>0 && $wagerValue>$beforeFinalJeopardyTotalPrize){
 			$notice='<p>SINCE YOUR CURRENT TOTAL PRIZE WINNINGS ARE ABOVE $0, PLEASE ENTER A VALUE THAT IS AT MOST EQUAL TO';
@@ -35,6 +35,9 @@
 			$('#wagerBox').find('p').eq(1).html($notice);
 		} else if ($beforeFinalJeopardyTotalPrize>0 && $wagerValue>$beforeFinalJeopardyTotalPrize){
 			$notice='<p>SINCE YOUR CURRENT TOTAL PRIZE WINNINGS ARE ABOVE $0, PLEASE MAKE SURE THE WAGER DOES NOT EXCEED YOUR TOTAL PRIZE WINNINGS.';
+			$('#wagerBox').find('p').eq(1).html($notice);
+		} else if ($beforeFinalJeopardyTotalPrize===0 && $wagerValue>10000){
+			$notice='<p>SINCE YOUR CURRENT TOTAL PRIZE WINNINGS ARE $0, PLEASE MAKE SURE THE WAGER DOES NOT EXCEED $10,000.';
 			$('#wagerBox').find('p').eq(1).html($notice);
 		} else {
 			$notice='<p>FINAL JEOPARDY! WAGER ACCEPTED!</p>';
@@ -68,6 +71,7 @@
 					
 					var $endTime=new Date();
 					var $inputValue=$('input').val().toUpperCase();
+					console.log($inputValue);
 					var $elapsedTime=Number(($endTime-$startTime))/1000;
 					var $finalJeopardyPrize;
 					var $rightOrWrong;
@@ -86,15 +90,49 @@
 					$finalPrize=$jeopardyTotalPrize+$doubleJeopardyTotalPrize+$finalJeopardyPrize; //Compute total prize 
 					localStorage.setItem('totalPrize',$finalPrize);
 					
-					var $displayBeforeFinalJeopardyPrize=$beforeFinalJeopardyTotalPrize;
-					var $sign='';
-					var $determineClass='positive';
+					var $displayBeforeFinalJeopardyPrize;
+					var $sign1;
+					var $determineClass1;
 					
-					if ($beforeFinalJeopardyTotalPrize<0){
+					var $displayFinalPrize;
+					var $sign2;
+					var $determineClass2;
+	
+					if ($beforeFinalJeopardyTotalPrize<0 && $finalPrize<0){ //The player enters the maximum allowable wager (positive equivalent +$10,000) & answers incorrectly.
 						$displayBeforeFinalJeopardyPrize=-$beforeFinalJeopardyTotalPrize;
-						$sign='-';
-						$determineClass='negative';
-					}
+						$sign1='-';
+						$determineClass1='negative';
+						
+						$displayFinalPrize=-$finalPrize;
+						$sign2='-';
+						$determineClass2='negative';
+					} else if ($beforeFinalJeopardyTotalPrize<0 && $finalPrize>0){ //The player enters the maximum allowable wager & answers correctly. 
+						$displayBeforeFinalJeopardyPrize=-$beforeFinalJeopardyTotalPrize;
+						$sign1='-';
+						$determineClass1='negative';
+						
+						$displayFinalPrize=$finalPrize;
+						$sign2='';
+						$determineClass2='positive';						
+					} else if ($beforeFinalJeopardyTotalPrize===0 && $finalPrize<0){
+						$sign2='-';
+						$determineClass2='negative';						
+					} else {
+						/*
+						For the following cases:
+						$beforeFinalJeopardyTotalPrize>0 && ($finalPrize===0 || $finalPrize>0) 
+						$beforeFinalJeopardyTotalPrize===0 && $finalPrize>0
+						1. the player bets 0 & answers correctly OR the player bets all & answers incorrectly.
+						2. the player bets the maximum allowable wager and answers correctly.
+						*/
+						$displayBeforeFinalJeopardyPrize=$beforeFinalJeopardyTotalPrize;
+						$sign1='';
+						$determineClass1='positive';						
+
+						$displayFinalPrize=$finalPrize;
+						$sign2='';
+						$determineClass2='positive';						
+					} 
 					
 					if ($inputValue===$correctAnswer){
 						$rightOrWrong='<p class="positive">CORRECT!</p>';
@@ -102,11 +140,11 @@
 						$rightOrWrong='<p class="negative">INCORRECT!</p><p> THE CORRECT ANSWER IS \'SALUTARY NEGLECT\'.</p>';
 					}
 					
-					$finalResults='<p>YOUR TOTAL PRIZE WINNINGS PRIOR TO FINAL JEOPARDY! WAS <span class='; 
-					$finalResults+='"'+$determineClass+'">'+$sign+'$'+$displayBeforeFinalJeopardyPrize+'</span>AND YOU WAGERED<span>$'+$wagerValue+'</span>';
-					$finalResults+='YOUR FINAL PRIZE WINNINGS ARE '+$sign+'$'+$finalPrize+'</p>';
+					$finalResults='<ul><li>TOTAL PRIZE WINNINGS PRIOR TO FINAL JEOPARDY!:<span class='; 
+					$finalResults+='"'+$determineClass1+'">'+$sign1+'$'+$displayBeforeFinalJeopardyPrize+'</span></li><li>YOUR WAGER:';
+					$finalResults+='<span class="positive">$'+$wagerValue+'</span></li><li>FINAL PRIZE WINNINGS:<span class="'+$determineClass2+'">'+$sign2+'$'+$displayFinalPrize+'</span></li></ul>';
 					
-					$finalJeopardyInfo='<div id="finalJeopardyInfo"><span>X</span>'+$rightOrWrong+$finalResults+'</div>';
+					$finalJeopardyInfo='<div id="finalJeopardyInfo"><span>X</span><h1>FINAL JEOPARDY! RESULTS</h1>'+$rightOrWrong+$finalResults+'</div>';
 					$('body').append($finalJeopardyInfo);
 					$('#finalJeopardyInfo').addClass('fadeIntoView');
 					
@@ -123,10 +161,11 @@
 							var $doubleJeopardyCorrect=localStorage.getItem('doubleJeopardyCorrect');
 							var $doubleJeopardyWinningPercentage=localStorage.getItem('doubleJeopardyWinningPercentage');
 							
-							$showGameStatsHTML='<div id="showGameStats"><h1>GAME STATS</h1><section><h2>JEOPARDY! STATS</h2><ul><li>PRIZE WINNINGS: $'+$jeopardyTotalPrize+'</li>';
+							$showGameStatsHTML='<div id="showGameStats"><h1>GAME STATS</h1><section><h1>JEOPARDY! STATS</h1><ul><li>PRIZE WINNINGS: $'+$jeopardyTotalPrize+'</li>';
 							$showGameStatsHTML+='<li> NUMBER CORRECT: '+$jeopardyCorrect+'</li><li>WINNING PERCENTAGE: '+$jeopardyWinningPercentage+'%</li></ul>';
-							$showGameStatsHTML+='</section><section><h2>DOUBLE JEOPARDY! STATS</h2><li>PRIZE WINNINGS: $'+$doubleJeopardyTotalPrize+'</li>';
-							$showGameStatsHTML+='<li>NUMBER CORRECT: '+$doubleJeopardyCorrect+'</li><li>WINNING PERCENTAGE: '+$doubleJeopardyWinningPercentage+'%</li></section></div>';
+							$showGameStatsHTML+='</section><section><h1>DOUBLE JEOPARDY! STATS</h1><li>PRIZE WINNINGS: $'+$doubleJeopardyTotalPrize+'</li>';
+							$showGameStatsHTML+='<li>NUMBER CORRECT: '+$doubleJeopardyCorrect+'</li><li>WINNING PERCENTAGE: '+$doubleJeopardyWinningPercentage+'%</li>';
+							$showGameStatsHTML+='</section></div>';
 							$('body').append($showGameStatsHTML);
 							$('#showGameStats').addClass('fadeIntoView');
 						}

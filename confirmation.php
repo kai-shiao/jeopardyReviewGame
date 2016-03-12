ace3047dda44bd02e6955d69f69400d0f6de8da2
Kai Shiao <kshiao13@gmail.com>
2016-03-12 09:34:41 -0800

    Revised comments.

---
+++ confirmation.php
@@ -0,0 +1,26 @@
+<?php
+#Save the newly received statistics sent to PHP from "finaljeopardy.html" via the POST method into corresponding PHP variables; 
+#they will be used to update existing stats.
+
+$scoreUpdate=$_POST["$score"];
+$correctUpdate=$_POST["$correct"];
+$answeredUpdate=$_POST["$answered"];
+$percentageUpdate=$correctUpdate/25;
+
+$score+=$scoreUpdate;
+$correct+=$correctUpdate;
+$answered+=$answeredUpdate;
+$percentage+=$percentageUpdate;
+
+$values=[$score,$correct,$answered,$percentage];
+$stats=["score","correct","percentage","answered"]; #Create an array to collect all the tag names of the XML elements to be accessed.
+
+$statSheet=new DOMDocument(); #Create a new DOMDocument object to be able to use DOM methods in PHP.
+$statSheet->load("data/playerstats.xml"); #The object is populated with the XML file containing player stats.
+
+$statSheet->getElementsByTagName($stats[0])->item(1)->nodeValue=$values[0];
+$statSheet->getElementsByTagName($stats[1])->item(1)->nodeValue=$values[1];
+$statSheet->getElementsByTagName($stats[2])->item(1)->nodeValue=$values[2];
+
+$loadStats->save('data/playerstats.xml');
+?>

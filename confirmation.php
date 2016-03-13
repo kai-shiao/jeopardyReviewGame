87dec5b03a3468879f12b2fae964a6a25187b0d5
Kai Shiao <kshiao13@gmail.com>
2016-03-13 15:24:40 -0700

    Finalized PHP code for updating XML file with player stats.

--- confirmation.php
+++ confirmation.php
@@ -7,40 +7,43 @@ $finalPrize=filter_input(INPUT_POST,"finalPrize");
 $jeopardyCorrect=filter_input(INPUT_POST,"jeopardyCorrect");
 $doubleJeopardyCorrect=filter_input(INPUT_POST,"doubleJeopardyCorrect");*/
 
-$contestantName="ere";
-$jeopardyTotalPrize="";
-$doubleJeopardyTotalPrize="";
-$finalPrize="";
-$jeopardyCorrect="";
-$doubleJeopardyCorrect="";
+$contestantName="errrr";
+$jeopardyTotalPrize="43";
+$doubleJeopardyTotalPrize="33";
+$finalPrize="33";
+$jeopardyCorrect="33";
+$doubleJeopardyCorrect="33";
 
 #Create a new DOMDocument object to be able to work with a XML document & use the XML DOM methods in PHP.
-$statSheet=new DOMDocument("1.0", "UTF-8");
-$statSheet->load("data/playerstats.xml");
-$statSheet->preserveWhiteSpace=false;
+$statSheet=new DOMDocument();
+#Important! These 2 following lines of code must be before the file is loaded so PHP knows that the XML file should be formatted.
+$statSheet->preserveWhiteSpace=false; 
 $statSheet->formatOutput=true;
+$statSheet->load("data/playerstats.xml");
 
-/*Set up a XML node with 4 child nodes inside to store player data.
+/*
+Set up a XML node with 5 child nodes inside to store player stats.
 
 Example:
 <playerSummary>
-				<NICOLE>
-						<jeopardyTotalPrize>15000</jeopardyTotalPrize>
-						<doubleJeopardyTotalPrize>15000</doubleJeopardyTotalPrize>
-						<finalPrize>30000</finalPrize>
-						<jeopardyCorrect>25</doubleJeopardyCorrect>
-						<doubleJeopardyCorrect>25</doubleJeopardyCorrect>
-				</NICOLE>
+	<player name="NICOLE">
+		<jeopardyTotalPrize>15000</jeopardyTotalPrize>
+		<doubleJeopardyTotalPrize>15000</doubleJeopardyTotalPrize>
+		<finalPrize>30000</finalPrize>
+		<jeopardyCorrect>25</doubleJeopardyCorrect>
+		<doubleJeopardyCorrect>25</doubleJeopardyCorrect>
+	</player>
 </playerSummary>
-
 */
 
 #Obtain the XML file's root element.
 $rootElement=$statSheet->getElementsByTagName("playerSummary")->item(0);
-$contestantNameElement=$statSheet->createElement("$contestantName");
-$rootElement->appendChild($contestantNameElement); #Append the XML node for contestant name as a direct descendant of the root element.
+$playerElement=$statSheet->createElement("player");
+$rootElement->appendChild($playerElement); #Append the XML node for contestant name as a direct descendant of the root element.
+$playerElement->setAttribute("name","$contestantName");
 
-#The contestant name node will have 4 children nodes: jeopardyTotalPrize, doubleJeopardyTotalPrize, finalPrize, jeopardyCorrect, doubleJeopardyCorrect.
+#The contestant name node will have 5 children nodes: jeopardyTotalPrize, doubleJeopardyTotalPrize, finalPrize, jeopardyCorrect, doubleJeopardyCorrect.
+
 $jeopardyTotalPrizeElement=$statSheet->createElement("jeopardyTotalPrize");
 $jeopardyTotalPrizeNodeValue=$statSheet->createTextNode($jeopardyTotalPrize);
 $jeopardyTotalPrizeElement->appendChild($jeopardyTotalPrizeNodeValue);
@@ -63,10 +66,9 @@ $doubleJeopardyCorrectElement->appendChild($doubleJeopardyCorrectNodeValue);
 
 $childNodes=array($jeopardyTotalPrizeElement,$doubleJeopardyTotalPrizeElement,$finalPrizeElement,$jeopardyCorrectElement,$doubleJeopardyCorrectElement);
 
-#Run a for-loop to assign the 5 newly created nodes as children of the contestantName node.
+#Run a for-loop to assign the 6 newly created nodes as children of the contestantName node.
 for ($i=0;$i<count($childNodes);$i++){
-	$contestantNameElement->appendChild($childNodes[$i]);
+	$playerElement->appendChild($childNodes[$i]);
 }
-
 $statSheet->save("data/playerstats.xml");
 ?>

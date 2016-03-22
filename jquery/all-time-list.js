4f6792e2f17b006293075f987b9cccfd11068572
Kai Shiao <kshiao13@gmail.com>
2016-03-21 20:00:11 -0700

    Added conditional statement to ensure that the windows stop loading if the user has not completed Jeopardy!

--- jquery/all-time-list.js
+++ jquery/all-time-list.js
@@ -1,21 +1,17 @@
 (function(){
 	$('document').ready(function(){
 		$.ajax({
-			type: 'POST',
 			url: 'data/playerstats.xml',
 			success: function(xml){ //'success' in a callback context is still valid and acceptable; .success() as a chained method following $.ajax() is deprecated.
 						var $k=0;
 						var $statsList=['jeopardyTotalPrize','doubleJeopardyTotalPrize','totalPrize',
 										'jeopardyCorrect','doubleJeopardyCorrect','totalCorrect'
 						];
-						var $numberOfStats=$statsList.length;
-						var $currentStat;
-						console.log($statsList.length);
 						
-						while ($k<$numberOfStats){
+						while ($k<$statsList.length){
 							var $playerNames=[]; //Initialize an empty array to hold all the names of players.
 							var $allObservations=[]; //Initialize an empty array to hold all the figures for a certain statistic sourced from the XML file.
-							$currentStat=$statsList[$k];
+							var $currentStat=$statsList[$k];
 							var $totalCount=$(xml).find($statsList[$k]).length; //Obtain the current statistic by using the sentry variable's vlaue as the array index to select the element.
 							//Find the total number of element nodes pertaining to that statistic; it will determine the number of times the for-loop will be run.
 							console.log($totalCount);
@@ -32,7 +28,7 @@
 							$allObservations.sort(function(a,b){ 
 									return b-a;
 							}).splice(9,$allObservations.length-10);
-								//Sort $allObservations in a descending fashion and select only the top 10 and remove all remaining elements.
+							//Sort $allObservations in a descending fashion and select only the top 10 and remove all remaining elements.
 							console.log($allObservations.length);
 							//Only keep the top 10 values and delete the rest.
 							//Start deleting from the element with an index number of 9 (but not including this element).
@@ -50,14 +46,14 @@
 								var $originalIndexPosition=$allObservations.indexOf($allObservations[$i]);
 								$dynamicallyGeneratedNameList+='<li>'+$playerNames[$originalIndexPosition]+'</li>';
 							}
-							console.log('reached line 45');
+							
 							var $htmlMarkupForNameList='<ol class="fadeIntoView">'+$dynamicallyGeneratedNameList+'</ol>';
 							var $htmlMarkupForStatList='<ul class="fadeIntoView">'+$dynamicallyGeneratedStatList+'</ul>';
 							$('body').find('section').eq($k).append($htmlMarkupForNameList).append($htmlMarkupForStatList);
-							console.log($k);
+							
 							$k++;
 						}
-					},
+					 },
 			fail: function(){
 					$('body').append('<p>SORRY, THERE WAS AN AJAX ERROR.</p>');
 				  }
@@ -71,9 +67,9 @@ function respondToClickEvent(e){
 	var $eventTarget=$(e.target);
 	var $eventTargetID=$(e.target).attr('id');
 	
-	function displayLoadingMessage(){
+	function displayMessage(){
 		var $cssTopValue=$eventTarget.offset().top+10; //Ensure that the loading message always appears in the window by finding the event target's top coordinate value.
-		$('body').append('<p>DATA UPDATED!....</p>');
+		$('body').append('<p>DATA UPDATED!</p>');
 		$('body').find('p').addClass('fadeIntoView').css('top',$cssTopValue);
 	
 		function fade(){
@@ -127,12 +123,13 @@ function respondToClickEvent(e){
 	}
 	
 	$.ajax({
-		type: 'POST',
 		url: 'data/playerstats.xml',
-		complete: displayLoadingMessage,
+		complete: displayMessage,
 		success: loadData,
 		fail: function(){
+				var $cssTopValue=$eventTarget.offset().top+10; //Ensure that the loading message always appears in the window by finding the event target's top coordinate value.
 				$('body').append('<p>SORRY, THERE WAS AN AJAX ERROR.</p>');
+				$('body').find('p').addClass('fadeIntoView').css('top',$cssTopValue);
 		}
 	});
 }

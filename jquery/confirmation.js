0301518d1ea8201685455990b5cffbf168fa85b6
Kai Shiao <kshiao13@gmail.com>
2016-03-21 20:04:43 -0700

    Added conditional statement to ensure that the windows stop loading the game curriculum review page if the user has not completed Jeopardy!

--- jquery/confirmation.js
+++ jquery/confirmation.js
@@ -1,6 +1,23 @@
 (function(){
 function redirectToGameCurriculum(){
-	location.href='game-curriculum.html';
+	if (localStorage.getItem('gameComplete')==='false' || 'null'){
+		$('body').append('<div id="incomplete"><span>X</span><p>YOU CANNOT REVIEW GAME MATERIAL PRIOR TO COMPLETING THE GAME.</p></div>');
+		$('#incomplete').addClass('fadeIntoView');
+		$('#incomplete').css('top',$('button').eq(0).offset().top+30);
+		
+		function closeNotification(){
+			$('#incomplete').removeClass('fadeIntoView').addClass('fadeFromView');
+			
+			function remove(){
+				$('#incomplete').first().remove();
+			}
+			setTimeout(remove,2000);
+		}
+		
+		$('#incomplete').first().on('click',closeNotification);
+	} else {
+		location.href='game-curriculum.html';
+	}
 }
 
 function redirectToJeopardy(){

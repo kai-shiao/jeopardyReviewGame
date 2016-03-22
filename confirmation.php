391d5aa8e2214a79e80c578cb30c6962d751b29f
Kai Shiao <kshiao13@gmail.com>
2016-03-21 19:56:29 -0700

    Added if-else statement to ensure that the user knows whether or not player data has been saved to the server.

--- confirmation.php
+++ confirmation.php
@@ -25,96 +25,98 @@ $totalPrize=filter_input(INPUT_POST,"totalPrize");
 $jeopardyCorrect=filter_input(INPUT_POST,"jeopardyCorrect");
 $doubleJeopardyCorrect=filter_input(INPUT_POST,"doubleJeopardyCorrect");
 $totalCorrect=$jeopardyCorrect+$doubleJeopardyCorrect;
-
-try{
-	#Create a new DOMDocument object to be able to work with a XML document & use the XML DOM methods in PHP.
-	$statSheet=new DOMDocument();
-	#Important! These 2 following lines of code must be before the file is loaded so PHP knows that the XML file should be formatted.
-	$statSheet->preserveWhiteSpace=false; 
-	$statSheet->formatOutput=true;
-	$statSheet->load("data/playerstats.xml");
-
-	/*
-	Set up a XML node with 5 child nodes inside to store player stats.
-
-	Example:
-	<playerSummary>
-		<player name="NICOLE">
-			<jeopardyTotalPrize>15000</jeopardyTotalPrize>
-			<doubleJeopardyTotalPrize>15000</doubleJeopardyTotalPrize>
-			<totalPrize>30000</totalPrize>
-			<jeopardyCorrect>25</doubleJeopardyCorrect>
-			<doubleJeopardyCorrect>25</doubleJeopardyCorrect>
-			<totalCorrect>51</totalCorrect>
-		</player>
-	</playerSummary>
-	*/
-
-	#Obtain the XML file's root element.
-	$rootElement=$statSheet->getElementsByTagName("playerSummary")->item(0);
-	$playerElement=$statSheet->createElement("player");
-	$rootElement->appendChild($playerElement); #Append the XML node for contestant name as a direct descendant of the root element.
-	$playerElement->setAttribute("name","$contestantName");
-
-	#The contestant name node will have 5 children nodes: jeopardyTotalPrize, doubleJeopardyTotalPrize, finalPrize, jeopardyCorrect, doubleJeopardyCorrect.
-
-	$jeopardyTotalPrizeElement=$statSheet->createElement("jeopardyTotalPrize");
-	$jeopardyTotalPrizeNodeValue=$statSheet->createTextNode($jeopardyTotalPrize);
-	$jeopardyTotalPrizeElement->appendChild($jeopardyTotalPrizeNodeValue);
-
-	$doubleJeopardyTotalPrizeElement=$statSheet->createElement("doubleJeopardyTotalPrize");
-	$doubleJeopardyTotalPrizeNodeValue=$statSheet->createTextNode($doubleJeopardyTotalPrize);
-	$doubleJeopardyTotalPrizeElement->appendChild($doubleJeopardyTotalPrizeNodeValue);
-
-	$totalPrizeElement=$statSheet->createElement("totalPrize");
-	$totalPrizeNodeValue=$statSheet->createTextNode($totalPrize);
-	$totalPrizeElement->appendChild($totalPrizeNodeValue);
-
-	$jeopardyCorrectElement=$statSheet->createElement("jeopardyCorrect");
-	$jeopardyCorrectNodeValue=$statSheet->createTextNode($jeopardyCorrect);
-	$jeopardyCorrectElement->appendChild($jeopardyCorrectNodeValue);
-
-	$doubleJeopardyCorrectElement=$statSheet->createElement("doubleJeopardyCorrect");
-	$doubleJeopardyCorrectNodeValue=$statSheet->createTextNode($doubleJeopardyCorrect);
-	$doubleJeopardyCorrectElement->appendChild($doubleJeopardyCorrectNodeValue);
-
-	$totalCorrectElement=$statSheet->createElement("totalCorrect");
-	$totalCorrectNodeValue=$statSheet->createTextNode($totalCorrect);
-	$totalCorrectElement->appendChild($totalCorrectNodeValue);
-
-	$childNodes=array($jeopardyTotalPrizeElement,$doubleJeopardyTotalPrizeElement,$totalPrizeElement,$jeopardyCorrectElement,$doubleJeopardyCorrectElement,$totalCorrectElement);
-
-	#Run a for-loop to assign the 6 newly created nodes as children of the contestantName node.
-	for ($i=0;$i<count($childNodes);$i++){
-		$playerElement->appendChild($childNodes[$i]);
-	}
-	$statSheet->save("data/playerstats.xml");
 	
-	$date=new DateTime();
-	$currentYear=$date->format("Y"); 
-	print <<<HERE
-	<p>YOUR SCORES HAVE BEEN SUCCESSFULLY SAVED AND WILL BE CONSIDERED FOR POSSIBLE RANKING IN THE ALL-TIME LIST.</p>
+if (isset($contestantName) && isset($jeopardyTotalPrize) && isset($doubleJeopardyTotalPrize) && 
+	isset($totalPrize) && isset($jeopardyCorrect) && isset($doubleJeopardyCorrect)){
+
+#Create a new DOMDocument object to be able to work with a XML document & use the XML DOM methods in PHP.
+$statSheet=new DOMDocument();
+
+#Important! These 2 following lines of code must be before the file is loaded so PHP knows that the XML file should be formatted.
+$statSheet->preserveWhiteSpace=false; 
+$statSheet->formatOutput=true;
+$statSheet->load("data/playerstats.xml");
+
+/*
+Set up a XML node with 5 child nodes inside to store player stats.
+
+Example:
+<playerSummary>
+	<player name="NICOLE">
+		<jeopardyTotalPrize>15000</jeopardyTotalPrize>
+		<doubleJeopardyTotalPrize>15000</doubleJeopardyTotalPrize>
+		<totalPrize>30000</totalPrize>
+		<jeopardyCorrect>25</doubleJeopardyCorrect>
+		<doubleJeopardyCorrect>25</doubleJeopardyCorrect>
+		<totalCorrect>51</totalCorrect>
+	</player>
+</playerSummary>
+*/
+
+#Obtain the XML file's root element.
+$rootElement=$statSheet->getElementsByTagName("playerSummary")->item(0);
+$playerElement=$statSheet->createElement("player");
+$rootElement->appendChild($playerElement); #Append the XML node for contestant name as a direct descendant of the root element.
+$playerElement->setAttribute("name","$contestantName");
+
+#The contestant name node will have 5 children nodes: jeopardyTotalPrize, doubleJeopardyTotalPrize, finalPrize, jeopardyCorrect, doubleJeopardyCorrect.
+
+$jeopardyTotalPrizeElement=$statSheet->createElement("jeopardyTotalPrize");
+$jeopardyTotalPrizeNodeValue=$statSheet->createTextNode($jeopardyTotalPrize);
+$jeopardyTotalPrizeElement->appendChild($jeopardyTotalPrizeNodeValue);
+
+$doubleJeopardyTotalPrizeElement=$statSheet->createElement("doubleJeopardyTotalPrize");
+$doubleJeopardyTotalPrizeNodeValue=$statSheet->createTextNode($doubleJeopardyTotalPrize);
+$doubleJeopardyTotalPrizeElement->appendChild($doubleJeopardyTotalPrizeNodeValue);
+
+$totalPrizeElement=$statSheet->createElement("totalPrize");
+$totalPrizeNodeValue=$statSheet->createTextNode($totalPrize);
+$totalPrizeElement->appendChild($totalPrizeNodeValue);
+
+$jeopardyCorrectElement=$statSheet->createElement("jeopardyCorrect");
+$jeopardyCorrectNodeValue=$statSheet->createTextNode($jeopardyCorrect);
+$jeopardyCorrectElement->appendChild($jeopardyCorrectNodeValue);
+
+$doubleJeopardyCorrectElement=$statSheet->createElement("doubleJeopardyCorrect");
+$doubleJeopardyCorrectNodeValue=$statSheet->createTextNode($doubleJeopardyCorrect);
+$doubleJeopardyCorrectElement->appendChild($doubleJeopardyCorrectNodeValue);
+
+$totalCorrectElement=$statSheet->createElement("totalCorrect");
+$totalCorrectNodeValue=$statSheet->createTextNode($totalCorrect);
+$totalCorrectElement->appendChild($totalCorrectNodeValue);
+
+$childNodes=array($jeopardyTotalPrizeElement,$doubleJeopardyTotalPrizeElement,$totalPrizeElement,
+                  $jeopardyCorrectElement,$doubleJeopardyCorrectElement,$totalCorrectElement
+			);
+
+#Run a for-loop to assign the 6 newly created nodes as children of the contestantName node.
+for ($i=0;$i<count($childNodes);$i++){
+	$playerElement->appendChild($childNodes[$i]);
+}
+$statSheet->save("data/playerstats.xml");
+	
+print <<<HERE
+<p>YOUR SCORES HAVE BEEN SUCCESSFULLY SAVED AND WILL BE CONSIDERED FOR POSSIBLE RANKING IN THE ALL-TIME LIST.</p>
 HERE;
-} catch (Exception $e){
-	$errorDetails=$e->getMessage();
+} else {
 	print <<<HERE
-	</section>
-		<p>THE FOLLOWING ERROR(S) OCCURRED:</p>
-		<p>$errorDetails</p>
-		<p>PLEASE GRAB AND EMAIL A SCREENSHOT OF THIS ERROR DESCRIPTION TO WESTON HILL AS SOON AS POSSIBLE.</p>
-	</section>
-HERE;
-} finally{
-	print <<<HERE
-	<p>WHAT WOULD YOU LIKE TO DO NOW?</p>
-	<button>REVIEW THE CURRICULUM COVERED IN THIS GAME.</button>
-	<button>PLAY AGAIN!</button>
-	<button>VIEW ALL-TIME LIST</button>
-	<footer>&copy;$currentYear. WESTON HILL. ALL RIGHTS RESERVED.</footer>
+	<p>THE PAGE DID NOT RECEIVE ANY PLAYER STATS FROM finaljeopardy.html AND THUS NO NEW DATA WAS ENTERED INTO THE SERVER DATA FILE.</p>	
 HERE;
 }
+
+$date=new DateTime();
+$currentYear=$date->format("Y"); 
+
+print <<<HERE
+<p>WHAT WOULD YOU LIKE TO DO NOW?</p>
+<button>REVIEW THE CURRICULUM COVERED IN THIS GAME.</button>
+<button>PLAY AGAIN!</button>
+<button>VIEW ALL-TIME LIST</button>
+<footer>&copy;$currentYear. WESTON HILL. ALL RIGHTS RESERVED.</footer>
+HERE;
 ?>
 </div>
+
 <script src="http://localhost/jeopardyReviewGame/jquery/jquery-2.2.2.min.js"></script>
 <script src="http://localhost/jeopardyReviewGame/jquery/confirmation.js"></script>
 </body>

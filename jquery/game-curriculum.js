f8092ca13a37fb24cded9020c4eb87e685168702
Kai Shiao <kshiao13@gmail.com>
2016-03-27 14:30:08 -0700

    Added mechanism to prevent cheating.

--- jquery/game-curriculum.js
+++ jquery/game-curriculum.js
@@ -2,22 +2,13 @@
 	function deliverContent(e){
 		e.preventDefault();
 		
-	//	if (localStorage.getItem('gameComplete')==='true'){
-			
-			if (e.target.href==='jeopardy-curriculum.html') {
-				$('h1').eq(1).text('JEOPARDY!').addClass('fadeIntoView')
-			} else if (e.target.href==='doublejeopardy-curriculum.html'){
-				$('h1').eq(1).text('DOUBLE JEOPARDY!').addClass('fadeIntoView')
-			} else {
-				$('h1').eq(1).text('');
-			}
-						$('section').load(e.target.href).addClass('fadeIntoView');
-
-			$('footer').removeClass('absolute').addClass('static');
-	/*	} else {
+		if (localStorage.getItem('gameComplete')==='true'){
+			$('section').load(e.target.href).addClass('fadeIntoView');
+		} else {
 			$('<p>YOU CANNOT REVIEW GAME MATERIAL PRIOR TO COMPLETING THE GAME!</p>').insertBefore('footer');
 			$('p').addClass('fadeIntoView');
-		}*/
+			$('footer').addClass('absolute');
+		}
 	}
 		
 	$('nav').find('ul').find('li').eq(2).find('ul').on('click',deliverContent);
